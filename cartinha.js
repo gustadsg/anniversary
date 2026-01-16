ea873c2911c690b797cd37d005538b4e3616fc92
[GDGOMES] Gustavo da Silva Gomes <gustavo.sgomes@embraer.com.br>
2026-01-16 08:30:48 -0300

    cartinha

---
+++ cartinha.js
@@ -0,0 +1,35 @@
+const pollys = {
+    latitude: -19.856720,
+    longitude: -43.945236
+}
+
+const base = {
+    id: 'pollymap',	// This should match the ID used in the DOM
+    projection: 'stereo',
+    showPlanets: true,
+    latitude: pollys.latitude,
+    longitude: pollys.longitude, 
+    clock: new Date(2025, 0, 21, 19, 0, 0, 0),
+    ground: false,
+    keyboard: false,
+    mouse: true,
+    az : 180,
+    negative : false,
+    showhelp: false,
+    showstarlabels: true,
+    negative: false,
+    showplanets: true,
+    showplanetlabels: true,
+    constellations: true, 
+}
+
+
+S(document).ready(function() {
+	S.virtualsky({
+        ...base,
+		id: 'pollymap',	// This should match the ID used in the DOM
+        clock: new Date(2026, 0, 21, 19, 0, 0, 0),
+        latitude: pollys.latitude,
+        longitude: pollys.longitude, 
+	});
+});
